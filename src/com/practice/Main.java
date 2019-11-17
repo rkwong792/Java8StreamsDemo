467646ea87c6397f1b23b0d0ea2f36a0ee3219b4
DESKTOP-5RABSME\Richard <rkwong792@gmail.com>
2019-11-17 07:05:05 -0500

    10 Examples

---
+++ src/com/practice/Main.java
@@ -0,0 +1,94 @@
+package com.practice;
+
+import java.awt.image.ImageProducer;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public Main() throws IOException {
+    }
+
+    public static void main(String[] args) throws IOException {
+        // 1. Integer Stream
+        IntStream
+                .range(1, 10)
+                .forEach(System.out::print);
+        System.out.println();
+
+
+        // 2. Integer Stream with skip
+        IntStream
+                .range(1, 10)
+                .skip(5)
+                .forEach(x -> System.out.println(x));
+        System.out.println();
+
+        // 3. Integer Stream with sum
+        System.out.println(
+                IntStream
+                        .range(1, 5)
+                        .sum());
+        System.out.println();
+
+        // 4. Stream.of, sorted and findFirst
+        Stream.of("Ava", "Aneri", "Alberto")
+                .sorted()
+                .findFirst()
+                .ifPresent(System.out::println);
+        System.out.println();
+
+        // 5. Stream from Array, sort, filter and print
+        String[] names = {"Al", "Ankit", "Kushal", "Brent", "Sarika", "Amanda", "Hans", "Starry", "Sherri"};
+        Arrays.stream(names)
+                .filter(x -> x.startsWith("S"))
+                .sorted()
+                .forEach(System.out::println);
+
+        // 6. average of squares of an int array
+        Arrays.stream(new int[]{2, 4, 6, 8, 10})
+                .map(x -> x * x)
+                .average()
+                .ifPresent(System.out::println);
+
+        // 7. Stream from List, filter, and print
+        List<String> people = Arrays.asList("Al", "Ankit", "Kushal", "Brent", "Sarika", "Amanda", "Hans", "Starry", "Sherri");
+        people.stream()
+                .map(String::toLowerCase)
+                .filter(x -> x.startsWith("a"))
+                .sorted()
+                .forEach(System.out::println);
+
+
+        // 8. Stream rows from text file, sort, filter and print
+        Stream<String> bands = Files.lines(Paths.get("bands.txt"));
+        bands
+                .sorted()
+                .filter(x -> x.length() > 13)
+                .forEach(System.out::println);
+        bands.close();
+        System.out.println();
+
+        // 9. Stream rows from text file and save to List
+        List<String> bands2 = Files.lines(Paths.get("bands.txt"))
+                .filter(x -> x.contains("Stones"))
+                .collect(Collectors.toList());
+        bands2.forEach(x -> System.out.println(x));
+        System.out.println();
+
+        // 10. Stream rows from CSV file and count
+        Stream<String> rows = Files.lines(Paths.get("data.txt"));
+        int rowCount = (int)rows
+                .map(x -> x.split(","))
+                .filter(x -> x.length == 3)
+                .count();
+        System.out.println(rowCount + " rows.");
+        rows.close();
+    }
+}
